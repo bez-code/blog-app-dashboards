4b027f3ee9f3067e481663b4fb7b7caf165f62ab
bez <Behzad.p.ashrafi@gmail.com>
2024-01-30 21:41:49 +0300

    Update

--- blog-dashboard/src/app/app.module.ts
+++ blog-dashboard/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// import { AngularFireModule } from '@angular/fire/compat'; 
+// import { AngularFirestoreModule } from '@angular/fire/compat/firestore'; 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CategoriesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // AngularFireModule.initializeApp(environment.firebaseConfig),
+    // AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ blog-dashboard/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig : {
+        apiKey: "AIzaSyD9oUKzQAxFYw27M5_gg36vNCXVp6R5Qqk",
+        authDomain: "blog-app-bc84a.firebaseapp.com",
+        projectId: "blog-app-bc84a",
+        storageBucket: "blog-app-bc84a.appspot.com",
+        messagingSenderId: "777802934382",
+        appId: "1:777802934382:web:db76fbf1f8f5e0bd519e7f"
+    }
+  };
