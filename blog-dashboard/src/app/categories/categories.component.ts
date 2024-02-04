f19b305626110ff19ca7b2d35115f591d76befea
bez <Behzad.p.ashrafi@gmail.com>
2024-02-04 20:47:12 +0300

    Added validation to the category input field and added a subCategory field to the database.

--- blog-dashboard/src/app/categories/categories.component.ts
+++ blog-dashboard/src/app/categories/categories.component.ts
@@ -7,17 +7,26 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent {
-  constructor(private afs: AngularFirestore) {}
+  constructor(private afs: AngularFirestore) { }
 
   onSubmit(formData: any) {
     let categoryData = {
-      category: formData.value.category,
+      category: formData.value.category
+    }
+    let subCategoryData = {
+      subCategory: 'subCategory1',
+
     };
     this.afs
       .collection('categories')
       .add(categoryData)
       .then((docRef) => {
         console.log('Category added with id', docRef);
+
+        this.afs.collection('categories').doc(docRef.id).collection('subCategories')
+          .add(subCategoryData).then(docRef1 => {
+            console.log(docRef1)
+          })
       })
       .catch((err) => {
         console.log(err);
