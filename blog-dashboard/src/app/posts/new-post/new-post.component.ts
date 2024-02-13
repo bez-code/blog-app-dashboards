48faa2843ac3e14d2b8d20548e37f06a5c7105a9
Behzad Ashrafi <79458932+bez-code@users.noreply.github.com>
2024-02-13 20:18:56 +0300

    Update new-post.component.ts

--- blog-dashboard/src/app/posts/new-post/new-post.component.ts
+++ blog-dashboard/src/app/posts/new-post/new-post.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class NewPostComponent {
 
+  permalink = '';
+  imageSrc: any = '';
+  selectedImage: any = ''
+
+  onTitleChange($event: any) {
+
+    const title = $event.target.value;
+    this.permalink = title.replace(/\s+/g, '-')
+
+  }
+
+  onImageChange($event: any) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imageSrc = e.target?.result;
+    }
+    reader.readAsDataURL($event.target.files[0])
+
+    this.selectedImage = $event.target.files[0];
+  }
+
 }
