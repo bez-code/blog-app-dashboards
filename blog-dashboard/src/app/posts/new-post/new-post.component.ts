ee9d058107b04be2a54f9ab03fba6eff01ff29a6
bez <Behzad.p.ashrafi@gmail.com>
2024-02-27 22:24:21 +0300

    Update post category selection to include category ID in the form value.

--- blog-dashboard/src/app/posts/new-post/new-post.component.ts
+++ blog-dashboard/src/app/posts/new-post/new-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Post } from 'src/app/models/post';
 import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
@@ -57,7 +58,24 @@ export class NewPostComponent implements OnInit {
 
   onSubmit(){
     console.log(this.postForm.value);
-    
+
+    let splited = this.postForm.value.category.split('-')
+
+    const postData: Post = {
+      title : this.postForm.value.title,
+      permalink : this.postForm.value.permalink,
+      excerpt: this.postForm.value.excerpt,
+      category : {
+        categoryId : splited[0],
+        category: splited[1]
+      },
+      postImgPath: '',
+      content: this.postForm.value.content,
+      isFeatured: false,
+      views: 0,
+      status: 'new',
+      createdAt: new Date(),
+    }
   }
 
 }
