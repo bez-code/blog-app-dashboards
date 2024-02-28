9b2c15a5a38bda5c841cdcab84d22b13485631ce
bez <Behzad.p.ashrafi@gmail.com>
2024-02-28 23:47:07 +0300

    Update component to display posts from Firestore

--- blog-dashboard/src/app/posts/all-posts/all-posts.component.ts
+++ blog-dashboard/src/app/posts/all-posts/all-posts.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-all-posts',
   templateUrl: './all-posts.component.html',
   styleUrls: ['./all-posts.component.css']
 })
-export class AllPostsComponent {
+export class AllPostsComponent implements OnInit {
 
+  postArray : Array<any> = [];
+
+  constructor(private postService:PostsService){}
+
+  ngOnInit(): void {
+
+    this.postService.loadData().subscribe(val => {
+      this.postArray = val;
+
+    })
+
+  }
 }
--- blog-dashboard/src/app/services/posts.service.ts
+++ blog-dashboard/src/app/services/posts.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,8 @@ export class PostsService {
 
   constructor(private storage: AngularFireStorage,
     private afs: AngularFirestore,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService,
+    private router: Router) { }
 
   uploadImage(selectedImage: any, postData: any) {
     const filePath = `postImg/${Date.now()}`
@@ -28,7 +31,20 @@ export class PostsService {
   saveData(postData: any) {
     this.afs.collection('posts').add(postData).then((docRef) => {
       this.toastr.success("Data insert successfully");
+      this.router.navigate(['/posts ']);
     })
   }
 
+  loadData() {
+
+    return this.afs.collection('posts').snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, data };
+        })
+      })
+    )
+  }
 }
